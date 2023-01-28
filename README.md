89ff877dbd043d78799af41822d96b824b9508d2
Samuel Mzee <samuelmzeee40@gmail.com>
2023-01-28 11:57:54 -0600

    Add scaffolding

---
+++ README.md
@@ -0,0 +1,8 @@
+# LpTestAPI written in express with TypeORM
+# Entire project took around 3.5 hrs to complete
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `data-source.ts` file
+3. Run `npm start` command
---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { Test } from "./entity/Test"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "....",
+    password: "....",
+    database: "....",
+    synchronize: true,
+    logging: false,
+    entities: [Test],
+    migrations: [],
+    subscribers: [],
+})
